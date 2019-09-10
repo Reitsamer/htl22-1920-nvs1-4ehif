b25a27d62d98ac99f969649cfeb1ac4b748f7dd9
ErikWohl <45392986+ErikWohl@users.noreply.github.com>
2019-09-10 11:47:30 +0200

    updated app.js

--- team/wohl/node-hello/app.js
+++ team/wohl/node-hello/app.js
@@ -4,3 +4,14 @@ var x = 10
 console.log('x: ' + x)
 console.log('x:', x)
 console.log(`x: ${x}`)
+
+x = 'SLOG'
+console.log('x:', x)
+
+var person = {
+    firstname: 'Erik',
+    lastname: 'Wohlrab',
+    age: '17',
+    address: '1210, Castlegasse'
+}
+console.log(`${person.firstname}, ${person.lastname} is ${person.age} years old.`)
