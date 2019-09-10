5f1207c9eda76e59c74a1752504c8b1e901cd392
Frostbiterino <45392972+Frostbiterino@users.noreply.github.com>
2019-09-10 11:51:18 +0200

    Update app.js

--- team/rose/node-hello/app.js
+++ team/rose/node-hello/app.js
@@ -1,1 +1,22 @@
 console.log('Hello 4EHIF')
+
+var x = 10;
+
+console.log('x: ' + x);
+console.log('x: ', x);
+console.log(`x:${x}`);
+
+
+x = 'SLOG';
+console.log('x:', x);
+
+
+var person = {
+    firstname: 'Marcus',
+    lastname: 'Rosel',
+    age:'18',
+}
+
+console.log(person);
+
+console.log(`${person.firstname} ${person.lastname} is ${person.age} years old.`);
