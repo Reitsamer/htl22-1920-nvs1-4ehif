472d930d0151b17d90781ec4d0bd101cd67de245
Erik Benke <erikbenke90@gmail.com>
2019-09-10 11:49:03 +0200

    various console.log  variants, first use of object

---
+++ team/BENK/node-hello/app.js
@@ -0,0 +1,21 @@
+console.log('Hello 4EHIF!');
+
+var x=10;
+
+console.log('x:'+x);
+console.log('x:',x);
+console.log(`x:${x}`);
+
+x='SLOG';
+
+console.log('x:', x);
+
+var person = {
+    firstname: 'Jesus Christ',
+    lastname: 'Talitanitus',
+    age: 22,
+    address: '1190, Budinskygasse'
+}
+
+console.log(`${person.firstname} ${person.lastname} is ${person.age} years old.`);
+
