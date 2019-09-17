6121fd13e822409a399eca7cbe516351e4492ad1
マリオ <Mario.Schrolmberger@studierende.htl-donaustadt.at>
2019-09-17 11:54:46 +0200

    17.09.2019

--- team/Schr/node-hello/app.js
+++ team/Schr/node-hello/app.js
@@ -1,16 +1,22 @@
-console.log('Hello World');
-var x = 10;
-console.log('x: ' + x);
-console.log('x:', x);
-console.log(`x: ${x}`);
+// console.log('Hello World');
+// var x = 10;
+// console.log('x: ' + x);
+// console.log('x:', x);
+// console.log(`x: ${x}`);
 
-x='SLOG'
+// x='SLOG'
+// console.log(x);
+
+// var person = {
+//     firstname:'Mario',
+//     lastname:'Schrolmbegrer',
+//     age:22,
+//     address:'1220, Addresse'
+// }
+// console.log(`${person.firstname}, ${person.lastname}, is ${person.age}`);
+
+const os = require('os');
+var x = os.userInfo();
+
 console.log(x);
-
-var person = {
-    firstname:'Mario',
-    lastname:'Schrolmbegrer',
-    age:22,
-    address:'1220, Addresse'
-}
-console.log(`${person.firstname}, ${person.lastname}, is ${person.age}`);
+console.log('Hallo, ' + x.username + '!');
