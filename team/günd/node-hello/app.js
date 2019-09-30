b11365c2ba2b24823c8ea6e8a60ecd5f20e45f60
kingyigi <45451429+kingyigi@users.noreply.github.com>
2019-09-30 21:08:21 +0200

    a

--- team/günd/node-hello/app.js
+++ team/günd/node-hello/app.js
@@ -1,8 +1,29 @@
+
 const os = require('os');
 const fs = require('fs');
 
-var x = os.userInfo()
+console.log('Hello 4EHIF!')
+
+
+var x = 10
+console.log('x:'+x)
+console.log('x:',x)
+console.log(`x:${x}`)
+
 
 console.log('Hallo, '+x.username + '!')
 
 fs.appendFileSync('message.txt',`Hallo, ${x.username}!`)
+
+x = 'SLOG'
+console.log('x:',x)
+
+var person = {
+    firstname: 'Yigi',
+    lastname: 'Gigi',
+    age: 22,
+    address: '1210, Hollywoodstraze'
+}
+
+console.log(`${person.firstname} ${person.lastname} is ${person.age} years old.`)
+
