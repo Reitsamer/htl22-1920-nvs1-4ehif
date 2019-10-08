5e601d0b2c56e0a0dcb59debad9a84adf5bf1d72
Wolf Reitsamer <reit@htl-donaustadt.at>
2019-10-08 11:43:06 +0200

    added yargs & fs, initial addNote functionality

--- src/node-notes/app.js
+++ src/node-notes/app.js
@@ -1,16 +1,20 @@
+const argv = require('yargs').argv
+
 const notes = require('./notes')
 
-const command = process.argv[2]
+console.log(argv)
 
-var title = 'Shopping'
-var body = 'Go to the supermarket'
+const command = argv._[0]
+
+var title = argv.title
+var body = argv.body
 
 if (command === 'create') {
   notes.addNote(title, body)
 } else if (command === 'read') {
-  notes.getNote(title)
+  var note = notes.getNote(title)
 } else if (command === 'list') {
-  notes.getAllNotes()
+  var allNotes = notes.getAllNotes()
 } else if (command === 'remove') {
   notes.removeNote(title)
 } else {
