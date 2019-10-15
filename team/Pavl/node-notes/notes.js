b9d8576240d14c48c66a54f0bdbdb047ffa8e729
PavloMarkovic <45437000+PavloMarkovic@users.noreply.github.com>
2019-10-15 10:10:15 +0200

    update

---
+++ team/Pavl/node-notes/notes.js
@@ -0,0 +1,31 @@
+const fs=require('fs');
+const os=require('os');
+
+const addNote=(title,body) => {
+ var newNote={
+    title:title,
+    body:body
+ };
+
+ fs.writeFileSync("notes.db",JSON.stringify(newNote));
+}
+
+const getNote = (title) => {
+
+}
+
+
+const getAllNotes = () => {
+    
+}
+
+const removeNotes = (title) => {
+    
+}
+
+module.exports={
+    addNote,
+    getNote,
+    getAllNotes,
+    removeNotes
+};
