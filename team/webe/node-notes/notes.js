73e3a10b87e7954aa0695a7e069f082d23ada79d
= <=>
2019-10-15 11:53:44 +0200

    reformat output with additional method add methods to find, remove and get Notes

--- team/webe/node-notes/notes.js
+++ team/webe/node-notes/notes.js
@@ -12,6 +12,9 @@ const fetchAllNotes = () => {
 const addNote = (title, body) => {
     var allNotes = fetchAllNotes();
 
+    if (allNotes.findIndex(n => n.title === title) > -1)
+        return false;
+
     var newNote = {
         title: title,
         body: body
@@ -20,23 +23,33 @@ const addNote = (title, body) => {
     allNotes.push(newNote);
 
     fs.writeFileSync('notes.json', JSON.stringify(allNotes));
+    return true;
 };
 
 const getNote = (title) => {
     return fetchAllNotes().find(i => i.title === title);
 };
 
-const getAllNotes = () => {
-
-};
+const getAllNotes = () => fetchAllNotes();
 
 const removeNote = (title) => {
+    var allNotes = fetchAllNotes();
+    var index = allNotes.findIndex(i => i.title === title);
+    if (index === -1)
+        return false;
+    allNotes.splice(index, 1);
+    fs.writeFileSync('notes.json', JSON.stringify(allNotes));
+    return true;
+};
 
+const printNote = (note) => {
+    console.log(`Title: ${note.title}\nBody: ${note.body}`);
 };
 
 module.exports = {
     addNote,
     getNote,
     getAllNotes,
-    removeNote
+    removeNote,
+    printNote
 };
