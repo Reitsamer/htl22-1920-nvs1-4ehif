1803de259b3346e1dcf0e4facc6f3aa74d3c40b5
TheBroGamersTv <superdante34@gmail.com>
2019-10-15 11:58:27 +0200

    commit

--- team/Duna/Node-nodes/app.js
+++ team/Duna/Node-nodes/app.js
@@ -7,17 +7,35 @@ var body = argv.body
 
 if(command==='create'){
     
-    notes.addNote(title,body)
+    if(notes.addNote(title,body))
+    {
+        console.log('note was added successfully.')
+    }
+    else{
+        console.log('note already exist')
+    }
 }else if(command==='read')
 {
    var note= notes.getNote(title)
+   console.log(note)
     
 }else if(command==='list')
 {
     var AllNotes= notes.getAllNotes();
+    AllNotes.forEach(n => console.log(n));
     
 }else if (command==='remove'){
-    notes.removeNote(title);
+    if( notes.removeNote(title))
+    {
+        console.log(`note was removed :`,title)
+    }else{
+        console.log('note could not be found')
+    }
+    
+}
+else if (command==='print'){
+    notes.printNote(title)
+    
 }else{
     console.error(`unknown command`,command);
     
--- team/Duna/Node-nodes/notes.js
+++ team/Duna/Node-nodes/notes.js
@@ -1,28 +1,60 @@
 const fs= require('fs');
+const fetchAllNotes=()=>{
+    try{
+    var existingNotes=fs.readFileSync('notes.json')
+    
+
+    return JSON.parse(existingNotes)
+    }
+    catch{
+        return []
+    }
+}
 const addNote=(title, body)=>{
+    var allNotes=fetchAllNotes();
+
+    if(allNotes.filter(n=>n.title===title).length>0){
+        return false
+    }
 
     var newNote={
         title: title,
         body:body,
     }
-
-    fs.writeFileSync('notes.db',JSON.stringify(newNote))
+    allNotes.push(newNote);
+    fs.writeFileSync('notes.json',JSON.stringify(allNotes))
+    return true;
 }
 
 const getNote=(title)=>{
-
+    var allNotes=fetchAllNotes();
+    var foundNotes= allNotes.filter(n=>n.title===title)
+    return foundNotes[0];
 }
 
-const getAllNotes=()=>{
+const getAllNotes=()=> fetchAllNotes();
 
-}
 const removeNote=(title)=>{
+    var allNotes= fetchAllNotes();
+    var newlist=allNotes.filter(n=>n.title!==title)
+    if(allNotes.length===newlist.length)
+    {
+        return false
+    }
+    fs.writeFileSync('notes.json',JSON.stringify(newlist))
+    return true
+}
 
+const printNote=(title)=>{
+    var allNotes=fetchAllNotes();
+    var sote=allNotes.filter(n=>n.title===title)
+    console.log(`der titel ist ${sote[0].title} und es wird gesagt ${sote[0].body}`)
 }
 
 module.exports={
     addNote, 
     getNote,
     getAllNotes,
-    removeNote
+    removeNote,
+    printNote
 }
