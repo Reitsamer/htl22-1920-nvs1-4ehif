0698111eeb2c97e6bc65204e20e0f6d417778766
Frostbiterino <45392972+Frostbiterino@users.noreply.github.com>
2019-10-15 11:51:29 +0200

    hii

--- team/rose/node-nodes/app.js
+++ team/rose/node-nodes/app.js
@@ -4,14 +4,20 @@ const notes = require('./notes');
 
 console.log(argv);
 
-const command = (process.argv[2])
+//const command = (process.argv[2])
 
-const title = 'Shopping';
-const body = 'Go to the supermarket';
+//const title = 'Shopping';
+//const body = 'Go to the supermarket';
+
+
+const command= argv._[0]
+var title=argv.title
+var body = argv.body
 
 if (command == 'create')
 {
     console.log('Creating note ...');
+    console.log(note);
     notes.addNote(title, body);
 }
 else if(command == 'read')
@@ -22,12 +28,22 @@ else if(command == 'read')
 else if(command == 'list')
 {
     console.log('Listing notes...')
-    notes.getAllNotes;
+    var allNotes = notes.getAllNotes();
+    console.log(allNotes);
+    allNotes.foreach(n => console.log(n))
 }
 else if(command == 'remove')
 {
     console.log('Removing notes...')  
-    notes.removeNote(title);
+   var success = notes.removeNote(title);
+   if(success === true)
+   {
+       console.log('Removed note with title: ', title);
+   }
+   else
+   {
+       console.log('Note with that title does not exist.')
+   }
 }
 else
 {
--- team/rose/node-nodes/notes.js
+++ team/rose/node-nodes/notes.js
@@ -1,29 +1,88 @@
 const fs = require('fs');
 
+const fetchAllNotes = () =>
+{
+    try
+    {  
+    var existingNotes = fs.readFileSync('notes.json');
+    return JSON.parse(existingNotes);
+    }
+    catch
+    {
+        return [];
+    }
+}
+
 const addNote = (title, body) => 
 {
+
+    var allNotes = fetchAllNotes();
+
+    if(allNotes.filter(n => n.title === title).length > 0)
+    {
+        return false;
+    }
+
     var newNote =
     {
         title : title,
         body : body,
     }
 
-    fs.writeFileSync('notes.db', JSON.stringify(newNote));
+    allNotes.push(newNote);
+
+    fs.writeFileSync('notes.json', JSON.stringify(allNotes));
 }
 
 const getNote = (title) => 
 {
+    // Challenge 
+    // 1. Read all notes
+
+
+    var allNotes = fetchAllNotes;
+
+    // 2. Find note with given title
+
+    var foundNotes = allNotes.filter(n => n.title === title);
+
+    // 3. Return the note
+
+    return foundNotes[0]; 
+
+    // 4. Print the note in app.js
 
 }
 
 const getAllNotes = () => 
 {
-
+    return fetchAllNotes;
 }
 
 const removeNote = (title) => 
 {
 
+    var allNotes = fetchAllNotes;
+
+    var remainingNotes = allNotes.filter(n => n.title !== title);
+   
+    if(allNotes.length == remainingNotes.length)
+    {
+        return false;
+    }
+
+    fs.writeFileSync('notes.json', JSON.stringify(remainingNotes));
+    return true;
+
+}
+
+// 1. Write new function 'printNote'
+// 2. printNote gets a note & prints it to the console
+
+const printNote = () =>
+{
+    var allNotes = fetchAllNotes;
+    console.log(allNotes);
 }
 
 // Exportieren zu app.js
@@ -32,5 +91,6 @@ module.exports =
     addNote,
     getNote,
     getAllNotes,
-    removeNote
+    removeNote,
+    printNote
 }
