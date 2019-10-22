055065134adbf2657e89632df30cc5643b2b0a4c
ErikWohl <45392986+ErikWohl@users.noreply.github.com>
2019-10-22 11:15:55 +0200

    Created node-test
    Den Vorbeitungstest begonnen und fertiggemacht

--- team/wohl/node-notes/app.js
+++ team/wohl/node-notes/app.js
@@ -9,18 +9,30 @@ const command = argv._[0]
 var title = argv.title
 var body = argv.body
 
+//Commands
+//node app.js create --title="test" --body="meh"
+
 if(command === 'create') {
     console.log('Creating note...')
-    notes.addNote(title, body)
+    var success = notes.addNote(title, body)
+    if(success === true) {
+        console.log('Note added successfully.')
+    } else {
+        console.log('Note with title already exists.')
+    }
 } else if (command === 'read') {
     console.log('Reading note...')
     var note = notes.getNote(title)
+    console.log(note)
 } else if (command === 'list') {
     console.log('Listing note...')
     var allNotes = notes.getAllNotes();
+    allNotes.forEach(n => console.log(n));
 } else if (command === 'remove') {
     console.log('Removing note...')
-    notes.removeNote(title);
+    console.log(notes.removeNote(title));
+} else if (command === 'print') {
+    notes.printNote(title);
 } else {
     console.error('Unknown command:', command)
 }
--- team/wohl/node-notes/notes.js
+++ team/wohl/node-notes/notes.js
@@ -1,29 +1,69 @@
 const fs = require('fs')
 const os = require('os')
 
+const fetchAllNotes = () => {
+    try {
+    var file = fs.readFileSync('notes.json')
+    return JSON.parse(file)
+    } catch(err) {
+        return []
+  }
+}
+
 const addNote = (title, body) => {
+
+    var allNotes = fetchAllNotes()
+
+    //Variante 1
+    if (allNotes.findIndex(n => n.title === title) > -1) {
+        return false;
+    }
+
+    //Variante 2
+    //if(allNotes.find(n => n.title === title).length > 0) {
+    //    return false;
+    //}
+
     var newNote = {
         title: title,
         body: body
     }
-    fs.writeFileSync('notes.db', JSON.stringify(newNote))
+
+    allNotes.push(newNote)
+
+    fs.writeFileSync('notes.json', JSON.stringify(allNotes))
+    return true;
 }
 
 const getNote = (title) => {
-    
+    var allNotes = fetchAllNotes()
+
+    //return allNotes.filter(n => n.title === title)[0]
+    return allNotes.find(n => n.title === title)
 }
 
-const getAllNotes = () => {
-    
-}
+const getAllNotes = () => fetchAllNotes()
 
 const removeNote = (title) => {
-    
+    var allNotes = fetchAllNotes()
+
+    var notes = allNotes.filter(n => n.title !== title)
+    if(allNotes.length - notes.length > 0) {
+        fs.writeFileSync('notes.json', JSON.stringify(notes))
+        return "Note found and deleted!"
+    } else {
+        return "No note found to delete!"
+    }
+}
+
+const printNote = (title) => {
+    console.log(getNote(title))
 }
 
 module.exports = {
     addNote,
     getNote,
     getAllNotes,
-    removeNote
+    removeNote,
+    printNote
 }
