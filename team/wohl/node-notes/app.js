0a60938cae49475db105bb7c9e6ae64067acc974
ErikWohl <45392986+ErikWohl@users.noreply.github.com>
2019-10-22 11:19:59 +0200

    Revert "Created node-test"
    This reverts commit 055065134adbf2657e89632df30cc5643b2b0a4c.

--- team/wohl/node-notes/app.js
+++ team/wohl/node-notes/app.js
@@ -9,30 +9,18 @@ const command = argv._[0]
 var title = argv.title
 var body = argv.body
 
-//Commands
-//node app.js create --title="test" --body="meh"
-
 if(command === 'create') {
     console.log('Creating note...')
-    var success = notes.addNote(title, body)
-    if(success === true) {
-        console.log('Note added successfully.')
-    } else {
-        console.log('Note with title already exists.')
-    }
+    notes.addNote(title, body)
 } else if (command === 'read') {
     console.log('Reading note...')
     var note = notes.getNote(title)
-    console.log(note)
 } else if (command === 'list') {
     console.log('Listing note...')
     var allNotes = notes.getAllNotes();
-    allNotes.forEach(n => console.log(n));
 } else if (command === 'remove') {
     console.log('Removing note...')
-    console.log(notes.removeNote(title));
-} else if (command === 'print') {
-    notes.printNote(title);
+    notes.removeNote(title);
 } else {
     console.error('Unknown command:', command)
 }
