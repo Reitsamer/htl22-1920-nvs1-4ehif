3e656b8426fd8fae14260913c1e5fadb90285a75
Peter Jovanovic <petertitan99999@gmail.com>
2019-10-22 10:16:34 +0200

    stuff

--- team/Jovan/node.notes/app.js
+++ team/Jovan/node.notes/app.js
@@ -1,15 +1,22 @@
+const argv = require('yargs').argv
+
 const notes = require('./notes')
-const command = process.argv[2]
-if(command==="create")
-{
-    console.log("Creating note...")
-    notes.addNote("SHopping", "Go to the Sumermarket")
-} else if (command==="read"){
-    console.log("Reading note...")
-} else if(command==="list"){
-    console.log("Listing notes..")
-} else if (command==='reomve'){
-    console.log("Removing note...")
-} else{
-    console.error("Unknown command:", command)
+
+console.log(argv)
+
+const command = argv._[0]
+
+var title = argv.title
+var body = argv.body
+
+if (command === 'create') {
+  notes.addNote(title, body)
+} else if (command === 'read') {
+  var note = notes.getNote(title)
+} else if (command === 'list') {
+  var allNotes = notes.getAllNotes()
+} else if (command === 'remove') {
+  notes.removeNote(title)
+} else {
+  console.error('Unknown command:', command)
 }
