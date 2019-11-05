a3787300b8a90abaffeba04a2304607f5df767c3
Peter Jovanovic <petertitan99999@gmail.com>
2019-11-05 09:17:33 +0100

    add missing package.jsons, fix bug in my person printer

--- team/Jovan/PersonPrinter/app.js
+++ team/Jovan/PersonPrinter/app.js
@@ -6,35 +6,35 @@ const fs = require('fs')
 const command = argv._[0]
 
 var input = argv.input
-var sortby=argv.sortby
-var order=argv.order
+var sortby = argv.sortby
+var order = argv.order
 
 const converter = require('./converter')
 
 if (command === 'convert') {
-    var putmeout=converter.convert(input)
-    if(putmeout.substring(0,11)==="Success = t"){
+    var putmeout = converter.convert(input)
+    if (putmeout.substring(0, 11) === "Success = t") {
         console.log(chalk.green(putmeout))
-    } else {       
+    } else {
         console.log(chalk.red(putmeout))
     }
-} else if(command==='pick'){
-    var putmeout=converter.pickRandom(input)
-    if(putmeout.substring(0,11)==="Success = t"){
-        var splitupboy=putmeout.substring(22)
-        process.stdout.write(chalk.green(putmeout.substring(0,22)))
+} else if (command === 'pick') {
+    var putmeout = converter.pickRandom(input)
+    if (putmeout.substring(0, 11) === "Success = t") {
+        var splitupboy = putmeout.substring(22)
+        process.stdout.write(chalk.green(putmeout.substring(0, 22)))
         converter.printPerson(splitupboy)
-    } else {       
+    } else {
         console.log(chalk.red(putmeout))
     }
-} else if(command==='print'){
-    var putmeout=converter.getSorted(input,sortby,order)
-    if(typeof putmeout!= String){
+} else if (command === 'print') {
+    var putmeout = converter.getSorted(input, sortby, order)
+    if (typeof putmeout != String) {
         console.log(chalk.green("Success = true, data: "))
         putmeout.forEach(part => {
             converter.printPerson(part)
-            });
-    } else {       
+        });
+    } else {
         console.log(chalk.red(putmeout))
     }
 }
