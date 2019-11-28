ed0ddcbcf82f16572556230f2c15672d615e9c45
Wolf Reitsamer <reit@htl-donaustadt.at>
2019-11-28 11:35:57 +0100

    weather forecast: final code

--- src/node-weather-forecast/app.js
+++ src/node-weather-forecast/app.js
@@ -1,53 +1,31 @@
 var request = require('request')
+var yargs = require('yargs')
+var forecast = require('./forecast')
+var { geocode } = require('./geocode')
 
-const url = 'https://api.darksky.net/forecast/ace7497ac9e8602309af76a56045a170/37.8267,-122.4233?lang=de&units=si'
+var location = yargs.argv.location
 
-request({ json: true, url: url }, (error, response, body) => {
-  // console.log('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', JSON.parse(body)); // Print the HTML for the Google homepage.
-
+geocode(location, (error, { name, latitude, longitude }) => {
+  
   if (error) {
-    console.error('Unable to connect to service.')
+    console.error(error)
     return
   }
 
-  if (response && response.statusCode >= 400) {
-    console.error('Unable to find location.')
-    return
-  }
+  forecast(latitude, longitude, (error, { summary, temperature, rainProbability}) => {
 
-  console.log(body.daily.data[0].summary)
+    if (error) {
+      console.error(error)
+      return
+    }
 
-  console.log(`It's currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`)
-
+    console.log(`Ort: ${name}`)
+    console.log('Zusammenfassung: ', summary)
+    console.log(`Die Temperatur beträgt: ${temperature} Grad Celsius`)
+    console.log(`Die Regenwahrscheinlichkeit liegt bei ${rainProbability}%.`)
+  })
 })
 
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Salzburg.json?access_token=pk.eyJ1IjoicmVpdCIsImEiOiJjanVkM3F5aGUwaHk1NGRxcDZrcjlzaXdkIn0.bQG57GOwvmpb_41o5Kge_Q'
 
-// CHALLENGE
-//
-// 1. Fire off a new request to the URL
-// 2. Parse the request
-// 3. Print place_name, save longitude & latitude (+print them)
-// 4. Test your work
 
-request({ url: geocodeURL, json: true }, (error, response) => {
-  if (error) {
-    console.error('Unable to connect to service.')
-    return
-  }
-
-  if (response.body.features.length === 0) {
-    console.error('Could not find location.')
-    return
-  }
-
-  const name = response.body.features[0].place_name
-  const longitude = response.body.features[0].center[0]
-  const latitude = response.body.features[0].center[1]
-
-  console.log('Name:', name)
-  console.log(`Latitude: ${latitude} -- Longitude: ${longitude}`)
-})
